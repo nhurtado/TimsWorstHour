78769b0056d354fe08634e33a0fd7e8a1519b653
nhurtado <nhurtado@miuandes.cl>
2019-11-09 17:36:34 -0300

    Enemies have HP now.

--- Assets/FlyingEnemyScript.cs
+++ Assets/FlyingEnemyScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FlyingEnemyScript : MonoBehaviour
 {
+    public int healthPoints = 3;
     public float speed = 2.5f;
     public float rightLimit = 5f;
     public float leftLimit = 5f;
