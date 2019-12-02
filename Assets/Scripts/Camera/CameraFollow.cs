cb60ad73c6479bedf4966ea8e8d5e23211648c60
nhurtado <nhurtado@miuandes.cl>
2019-12-02 20:29:28 -0300

    Made the camera snappier. Fixed the UI.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,22 +6,20 @@ public class CameraFollow : MonoBehaviour
 {
 
     GameObject player;
-    Rigidbody2D rb;
     public float playerVelocityOffset = 1.5f;
-    public float cameraSpeed = 1;
+    public float cameraSpeed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        rb = player.transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 st = transform.position;
-        Vector3 ed = player.transform.position + new Vector3(rb.velocity.x * playerVelocityOffset, 2.5f, -1);
+        Vector3 ed = player.transform.position + new Vector3(0, 2.5f, -1);
         transform.position = Vector3.Lerp(st, ed, Time.deltaTime * cameraSpeed);
     }
 }
