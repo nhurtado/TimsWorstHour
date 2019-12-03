76df28e35c038b7b1dbea753730625dcda163301
nhurtado <nhurtado@miuandes.cl>
2019-12-03 18:51:22 -0300

    Added Mantained Button.

---
+++ Assets/Scripts/ButtonMantainedScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMantainedScript : MonoBehaviour
+{
+    GameObject colGameObject;
+    public Transform target;
+
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        colGameObject = collision.gameObject;
+        if (colGameObject.tag == "Player" || colGameObject.tag == "IceBall" || colGameObject.tag == "GrabbableObject")
+        {
+            if (target.gameObject.activeSelf)
+            {
+                target.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (colGameObject.tag == "Player" || colGameObject.tag == "IceBall" || colGameObject.tag == "GrabbableObject")
+        {
+            target.gameObject.SetActive(true);
+        }
+    }
+
+
+
+}
