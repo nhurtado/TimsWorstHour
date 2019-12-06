ef4affc75e6ea02d425042cdbf982d1bce9d51f4
nhurtado <nhurtado@miuandes.cl>
2019-12-05 22:54:51 -0300

    Added urn frames. Restoring level 1.

---
+++ Assets/UrnScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UrnScript : MonoBehaviour
+{
+    public Animator anim;
+    public bool playedOnce = false;
+    void Start()
+    {
+        //anim.Play("UrnBreaking");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !playedOnce)
+        {
+            anim.Play("UrnBreaking");
+        }
+    }
+}
