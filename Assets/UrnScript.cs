f1fe2ed4abb6dc40af0e7b94d0b4d35ada2fd6e2
nhurtado <nhurtado@miuandes.cl>
2019-12-06 01:44:14 -0300

    Level 1 almost finished. Missing tutorial controls.

--- Assets/UrnScript.cs
+++ Assets/UrnScript.cs
@@ -6,16 +6,26 @@ public class UrnScript : MonoBehaviour
 {
     public Animator anim;
     public bool playedOnce = false;
-    void Start()
-    {
-        //anim.Play("UrnBreaking");
-    }
+    public GameObject speechBubbleGrawlix;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player" && !playedOnce)
         {
+            playedOnce = true;
             anim.Play("UrnBreaking");
+            collision.gameObject.GetComponent<PlayerScript>().canMove = false;
+            StartCoroutine("LoadNextLevelInSeconds");
         }
+    }
+
+    IEnumerator LoadNextLevelInSeconds()
+    {
+        yield return new WaitForSeconds(8);
+        speechBubbleGrawlix.SetActive(true);
+        yield return new WaitForSeconds(2.5f);
+        speechBubbleGrawlix.SetActive(false);
+        yield return new WaitForSeconds(2.5f);
+        Debug.Log("Load level");
     }
 }
