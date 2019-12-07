aa3b15fa59af6e575c48e9964e5a2cc49a3e40e0
CristobalLara <cristobal.p.lara@gmail.com>
2019-12-07 03:52:24 -0300

    Level 3 Boss Trigger

---
+++ Assets/UfoTriggerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoTriggerScript : MonoBehaviour
+{
+    public GameObject Trigger;
+    public GameObject DemonUFO;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            DemonUFO.SetActive(true);
+            Trigger.SetActive(false);
+            
+        }
+    }
+}
